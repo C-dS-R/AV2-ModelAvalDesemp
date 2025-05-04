91e5f31d6d52e6d996852a190d71156a162f3eba
C-dS-R <carolinadesouzaribeiro@hotmail.com>
2025-05-04 11:59:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,71 @@
-# bibliotecas
+##### Bibliotecas
 import pandas as pd
 import statsmodels.api as sm
-#minhas funcoes
-# from funcoes.parte1.estatisticas_iniciais import calc_estatisticas_iniciais
 
-#LEITURA DOS DADOS
-dados = pd.read_csv('dataset_5.csv', sep=',', encoding='latin1')
+##### DADOS
+dados = pd.read_csv('dataset_5.csv', sep=',', encoding='latin1') # LEITURA
+dados_dropped = dados.dropna() # cópia dos dados sem NAs
+labels_colunas = dados.columns.values.tolist() #['cpu_cores', 'ram_gb', 'latencia_ms', 'armazenamento_tb', 'sistema_operacional', 'tipo_hd', 'tipo_processador', 'tempo_resposta']
 
-# obtem labels das colunas
-# labels = dados.columns.values.tolist()
-labels = ['cpu_cores', 'ram_gb', 'latencia_ms', 'armazenamento_tb', 'sistema_operacional', 'tipo_hd', 'tipo_processador', 'tempo_resposta']
+#########################################################
+################## INICIO DAS QUESTÕES ##################
+#########################################################
+# Parte I – Análise Estatística:
+print('------Parte 1: Análise Estatística Inicial-------')
+# 1.Realize uma análise estatística inicial do conjunto de dados, obtendo medidas como média, mediana, mínimo, máximo, dentre outras medidas que julgue adequadas. Em seguida, interprete os resultados, comentando sobre a distribuição e as tendências centrais das variáveis.
+def p1_q1():
+    print(dados.describe())
+    #TODO: interpretação dos resultados, comentando sobre a distribuição e as tendências centrais das variáveis.
+# parte1_q1() #TODO: descomentar uma vez que finalizado
+
+# Parte II – Modelo e Diagnóstico
+print('\n\n\n------Parte 2: Modelo e Diagnóstico-------')
+#  2. Ajuste um modelo de regressão linear múltipla considerando:
+def p2_q2():
+    # Variável dependente: tempo_resposta
+    y = dados_dropped['tempo_resposta']
+    # Variáveis explicativas: demais variáveis
+    X = dados_dropped[labels_colunas].drop(columns=['tempo_resposta'])
+
+    # Obtem colunas categóricas ('sistema_operacional', 'tipo_hd', 'tipo_processador')
+    cols_categoricas = X.select_dtypes(include=['object']).columns.tolist()
+
+    # Cria variaveis dummy para as colunas categóricas (obs: usa a 1a categoria como base)
+    X = pd.get_dummies(X, columns=cols_categoricas, drop_first=True)
+
+    # Adiciona intercepto
+    X = sm.add_constant(X)
+
+    # Cria e treina o modelo
+    modelo = sm.OLS(y, X).fit()
+
+    # printa resumo dos resultados do modelo
+    print(modelo.summary())
+
+    #retorna o modelo
+    return modelo
+p2_q2() #TODO: descomentar uma vez que finalizado
+
+# 3. Informe (de acordo com as técnicas, abordagens e testes vistos em sala de aula):
+#  ● O valor do intercepto e dos coeficientes estimados.
+#  ● O valor de R² e R² ajustado.
+#  ● Os valores de testes para interpretação dos coeficientes e do modelo de forma global.
+# 4. Sobre as variáveis categóricas:
+#  ● Como você tratou as variáveis categóricas do seu dataset (Mencione também quais foram as variáveis categóricas)?
+#  ● Qual categoria base foi considerada para cada uma?
+#  ● Interprete os coeficientes associados a essas categorias. 
+# 5. Faça o diagnóstico de multicolinearidade:
+#  ● Calcule o(s) fator(es) que auxiliam no diagnóstico de multicolinearidade, de acordo com o que foi visto em sala de aula.
+#  ● Interprete: existe multicolinearidade? Alguma ação seria necessária? 
+# 6. Faça o diagnóstico de heterocedasticidade:
+#  ● Elabore os gráficos e testes pertinentes para o diagnóstico da heterocedasticidade.
+#  ● Interprete os resultados.
 
 
-########### PARTE 1 - Análise Estatística ###########
-print('------------------ PARTE 1 - Análise Estatística ------------------')
-print(dados.describe())
-#TODO: Interpretar resultados
-
-
-
-########### PARTE 2 - Modelo e Diagnóstico ###########
+# Parte III – Análise Crítica
+# 7. Compare dois modelos:
+#  ● Modelo 1: com todas as variáveis.
+#  ● Modelo 2: excluindo uma variável (ou variáveis). Explique o motivo da exclusão dessa variável.
+#  ● Compare o R² ajustado e o teste F entre os modelos.
+#  ● Justifique qual modelo você recomendaria utilizar.
+#  ● Quais ações práticas você sugeriria para melhorar o tempo de resposta do sistema? Considere aspectos como escolha de hardware e possíveis gargalos identificados
