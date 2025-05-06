d4f2a4d1379d0ac1c8d9caccf7c92ac2e5e5ff84
C-dS-R <carolinadesouzaribeiro@hotmail.com>
2025-05-06 10:22:53 -0300

    aa

--- maint3.py
+++ maint3.py
@@ -50,17 +50,16 @@ def run_log_model(df_limpo: pd.DataFrame) -> sm.regression.linear_model.Regressi
     return modelo
 
 
-result_str = '' #para os logs
-
-#################################### INICIO
+#################################### INICIO ####################################
 
 # LEITURA DE DADOS
-df = pd.read_csv("dataset_5.csv", sep=",")
+df = pd.read_csv('dataset_5.csv', sep=",")
 
-quant_linhas_og = len(df) #quantidade original de linhas
+#quantidade original de linhas
+quant_linhas_og = len(df)
 
 #infos iniciais
-result_str += (f'--- Infos do Dataset\n{df.isnull().sum().to_string()} dados faltantes ({df.isnull().sum() / quant_linhas_og * 100}% do total)\n')
+result_str = (f'--- Infos do Dataset\n{df.isnull().sum().to_string()} dados faltantes ({df.isnull().sum() / quant_linhas_og * 100}% do total)\n')
 
 #pre processamento
 df_limpo = df.dropna().copy()
@@ -69,11 +68,11 @@ linhas_a_menos = quant_linhas_og - quant_linhas_limpo
 
 result_str += (f"---Tratamento de dados NA\n{linhas_a_menos} linhas a menos ({(linhas_a_menos / quant_linhas_og) * 100}% do total)\nNova shape: {df_limpo.shape}")
 
-#identificar vars
-num_cols = df_limpo.select_dtypes(include=["number", "bool"]).columns.tolist()
-cat_cols = df_limpo.select_dtypes(include="object").columns.tolist()
+#identifica colunas numericas/categóricas
+cols_num = df_limpo.select_dtypes(include=["number", "bool"]).columns.tolist()
+cols_cat = df_limpo.select_dtypes(include="object").columns.tolist()
 
-result_str += (f"\n--- Vars numéricas ---\n{num_cols}\n--- Vars categóricas ---\n{cat_cols}")
+result_str += (f"\n--- Vars numéricas ---\n{cols_num}\n--- Vars categóricas ---\n{cols_cat}")
 
 
 #####PARTE 1
@@ -81,19 +80,19 @@ result_str += (f"\n\n--- PARTE 1 ---\n\n--- Estatística descritiva (N={quant_li
 result_str += df_limpo.describe().to_markdown()
 
 ######PARTE 2
-result_str += ("\n\n--- PARTE 2 ---\n\n--- Modelo ---\n")
+result_str += ("\n\n--- PARTE 2 ---\n\n--- Modelo 1 ---\n")
 
 # one-hot encoding
 X_cat = pd.DataFrame(index=df_limpo.index)
 
-X_num = df_limpo[num_cols]
-X_full = pd.concat([X_num, X_cat], axis=1)
-X_full_com_intercepto = sm.add_constant(X_full, has_constant="add")
+X_numericas = df_limpo[cols_num]
+X_todas = pd.concat([X_numericas, X_cat], axis=1)
+X_todas_com_intercepto = sm.add_constant(X_todas, has_constant="add")
 y = df_limpo['tempo_resposta']
 
 
 #cria e treina o modelo
-modelo = sm.OLS(y, X_full_com_intercepto).fit(cov_type="HC3")
+modelo = sm.OLS(y, X_todas_com_intercepto).fit(cov_type="HC3")
 
 #mostra resultados do modelo
 result_str += (modelo.summary().as_text()) + "\n"
@@ -106,15 +105,15 @@ grafico_influencia(modelo, "influence.png", pasta_output)
 result_str += ("\n\n--- Diagnóstico VIF (Modelo 1)\n")
 vif_df = pd.DataFrame(
     {
-        "Variavel": X_full.columns,
-        "VIF": [variance_inflation_factor(X_full.values, i) for i in range(X_full.shape[1])],
+        "Variavel": X_todas.columns,
+        "VIF": [variance_inflation_factor(X_todas.values, i) for i in range(X_todas.shape[1])],
     }
 )
 result_str += (vif_df.to_markdown(index=False)) + "\n"
 
 # Diagnóstico Breusch-Pagan
 result_str += ("--- Heterocedasticidade (Breusch-Pagan)\n")
-bp = het_breuschpagan(modelo.resid, X_full_com_intercepto)
+bp = het_breuschpagan(modelo.resid, X_todas_com_intercepto)
 for label, val in zip(["LM estatística","LM p-valor","F estatística","F p-valor"], bp):
     result_str += (f"{label}: {val:.4f}\n")
 result_str += (
@@ -137,7 +136,7 @@ if not vars_insignificantes.empty: #se houver variavel insignificante
     var_a_remov = vars_insignificantes.idxmax()
 
     result_str += (f"\n- Variavel a remover: {var_a_remov} (p-valor={vars_insignificantes.max()})\n")
-    X_red = X_full_com_intercepto.drop(columns=[var_a_remov])
+    X_red = X_todas_com_intercepto.drop(columns=[var_a_remov])
 
     #cria e treina modelo
     modelo2 = sm.OLS(y, X_red).fit(cov_type="HC3")
